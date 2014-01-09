34ad48bbd753e54e47e60c68f563cc90d63300aa
dmw <d.millerwatts@gmail.com>
2014-01-08 19:21:54 -0500

    Update speech-recognizer.js
    commenting done

--- final-RedactionPoetics-fin/speech-recognizer.js
+++ final-RedactionPoetics-fin/speech-recognizer.js
@@ -13,17 +13,17 @@ var started = 0;
         recognition.continuous = true;
         recognition.interimResults = true;
 
-        var interimResult = '';
+        var interimResult = '';		//variables for initial recognized speech, eventual rhymed speech 
         var textArea = $('#speech-page-content');
         var rhymeArea = $('#speech-page-rhyme');
-        var textAreaID = 'speech-page-content';
+        var textAreaID = 'speech-page-content';   //string identifier for text area - necessary for function call later on
 
-        $('.speech-mic').click(function(){ //this is a reqmt of javascript api tts/stt functioning--tried to remove, as other have also tried, to no avail. Will try later, when more advanced.
+        $('.speech-mic').click(function(){ //speech recogfunction executed when click on microphone
         //$('.start').click(function(){
 	    if( started == 0 ) {
             	startRecognition();
 		started=1;
-		var text1 = document.getElementById('speech-page-content');
+		var text1 = document.getElementById('speech-page-content'); //
 		text1.value="";	
 	    }
 	    else {
@@ -32,20 +32,24 @@ var started = 0;
 		var text1 = document.getElementById('speech-page-content'); //the recognized speech, such as it is...
 		var text2 = document.getElementById('speech-page-rhyme');  ///to be rhymed
 		var rhymes;
-		var words = text1.value.split(" "); 
+		var words = text1.value.split(" "); //pulling out words from text1 and putting them into an array.
 		//var tts = new GoogleTTS('en');
-        text2.value = "";
-        for(i=0; i<words.length; i++) {
-		  	rhymes = lex.rhymes(words[i]);
-            var pos = Math.floor(Math.random()*rhymes.length); // Random selection of rhymed word-uses the length of the array to limit # of rhymed words (math.floor), & picks by position
-			if( rhymes[pos] != 'undefined' && rhymes[pos] ) {
-				text2.value = text2.value + rhymes[pos] + ' ';
+        	text2.value = "";  
+        	for(i=0; i<words.length; i++) {   //for the array words, go through and find rhymed words for each.
+			rhymes = lex.rhymes(words[i]); // for words[i], and element of words array--single word--
+			//find all the rhymed words in the lexicon & put them into the array called rhymes
+            		var pos = Math.floor(Math.random()*rhymes.length); // the position of the randomly selected 
+            		//(floor rounds down the fraction from random) rhymed word
+            		//within the rhymes array; pull out a random rhymed word from entire array of rhymed words
+			if( rhymes[pos] != 'undefined' && rhymes[pos] ) {    //prevent results that came back as "undefined" from being spoken
+				text2.value = text2.value + rhymes[pos] + ' '; //add the rhymed word to the text2 area, separated by speces
 				// speak(rhymes[0]);   // Ignore because I use the full string once completed
 			}
 		}
-		meSpeak.speak(text2.value, { variant:'f5', speed:'100', wordgap:'2' } );//tts capability, voice selection, speed, gap btwn wds
+		meSpeak.speak(text2.value, { variant:'f5', speed:'100', wordgap:'2' } );
+		//tts capability, voice selection, speed, gap btwn wds
 		started=0;	
-		}
+	}
         });
 
         $('.speech-mic-works').click(function(){ //part of the required workflow tts/stt--There was some commentary online abt how to get around it, but I'm not advanced enough yet to attempt it.
@@ -53,7 +57,7 @@ var started = 0;
             recognition.stop();
         });
 
-        var startRecognition = function() {
+        var startRecognition = function() {  //speech recognition of rhymed words
             $('.speech-content-mic').removeClass('speech-mic').addClass('speech-mic-works');
             textArea.focus();
             recognition.start();
